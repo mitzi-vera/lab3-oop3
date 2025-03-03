c4541c15a70fc64c025e950e15f465fb452a89c4
Mitzi Vera <144722779+mitzi-vera@users.noreply.github.com>
2025-03-02 21:27:25 -0700

    updated: DictionaryADT, Insert method

--- lab3StartingCode/src/utilities/DictionaryADT.java
+++ lab3StartingCode/src/utilities/DictionaryADT.java
@@ -3,14 +3,39 @@ package utilities;
 /**
 * DictionaryADT.java
 *
-* @author your name
+* @author Alessandra Nicole Claur, Lulubelle Fontelo, Mitzi Vera Escartin, Gabriel Ira Siwa
 * @version major.minor revision number starting at 1.0
+* @date March 6, 2025
 * 
-* Class Definition: This interface represents the public contract for the
-* implementation of Dictionary for the DictionaryADT Lab. 
+* @param <K> the type of keys maintained in this dictionary
+* @param <V> the type of mapped values
 */
 
 public interface DictionaryADT<K,V>
 {
 
+	/**
+	 * Inserts an instance of a key-value pair into the dictionary.
+	 * Pre-condition: the key must be unique (should not exist in the current dictionary)
+	 * Post-condition: the key-value pair is added, and the dictionary size increases
+	 * @param key they key to the instance
+	 * @param value the value associated with the key
+	 * @throws DuplicateKeyException if the key already exists
+	 * @throws IllegalArgugmentException if the key or value is null
+	 * */
+	
+	void insert(K key, V value) throws DuplicateKeyException, IllegalArgumentException;
+	
+	
+	/**
+	 * Please update the comments and the method parameters and exceptions as needed */
+	void remove();
+	
+	/**
+	 * Please update the comments and the method parameters and exceptions as needed */
+	void update();
+	
+	/**
+	 * Please update the comments and the method parameters and exceptions as needed */
+	void lookup();
 }
