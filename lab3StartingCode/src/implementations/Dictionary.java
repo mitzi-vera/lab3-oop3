8ef2218abfb7cbec02f287de80dbedd3a6fdfcf6
LulubelleF <Lulubelle.Fontelo@edu.sait.ca>
2025-03-11 11:40:02 -0600

    dictionaryTest update

--- lab3StartingCode/src/implementations/Dictionary.java
+++ lab3StartingCode/src/implementations/Dictionary.java
@@ -2,15 +2,62 @@ package implementations;
 
 import java.util.ArrayList;
 
+import exceptions.DuplicateKeyException;
 import utilities.DictionaryADT;
 
-public class Dictionary<K,V> implements DictionaryADT<K,V>
-{
-	// constant
-	private static final int DEFAULT_SIZE = 10;
-	
-	// index indicates pairing, e.g. keys[1] is stored at values[1]
-	private ArrayList<K> keys;
-	private ArrayList<V> values;
+public class Dictionary<K, V> implements DictionaryADT<K, V> {
+    private static final int DEFAULT_SIZE = 10;
+    private ArrayList<K> keys;
+    private ArrayList<V> values;
 
+    public Dictionary() {
+        this(DEFAULT_SIZE);
+    }
+
+    public Dictionary(int size) {
+        keys = new ArrayList<>(size);
+        values = new ArrayList<>(size);
+    }
+
+    @Override
+    public V remove(K key) {
+        int index = keys.indexOf(key);
+        if (index != -1) {
+            V removedValue = values.get(index); // Store the value before removal
+            keys.remove(index);
+            values.remove(index);
+            return removedValue; // Return the removed value
+        }
+        return null; // Key not found
+    }
+
+
+
+	@Override
+	public void create(int size)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean insert(K key, V value) throws DuplicateKeyException
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(K key, V value)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V lookup(K key)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
